94ca61cbe253e08dedc0a239e4edd2a82305aa80
Sayandip Halder <sayandiph4@gmail.com>
2022-01-16 21:29:57 +0530

    add chessboard.jsx and chess.js, and render chessboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Chessboard from "chessboardjsx";
+
+const Chess = require('chess.js');
+const chess = new Chess();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-centre">
+      <h1>Two player Chess</h1>
+      <Chessboard
+        width={400}
+        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+      />
     </div>
   );
 }
